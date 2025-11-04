46b1c18f2291fa724930b62a2dd94581bd3aa519
sid2417 <siddhartha.java9398@gmail.com>
2025-11-04 16:52:07 +0530

    This is rawcode of vpc module

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,23 @@ output "default_vpc" {
   
 }
 
-
 output "main_vpc_id" {
     value = aws_vpc.main.id
   
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public_subnets.id
+  
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private_subnets.id
+  
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database_subnets.id  
+}
+
 
